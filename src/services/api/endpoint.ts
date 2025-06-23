9df299d0fe88263ae8828ef30cfccaed99649786
H4ngL <a01031367140@gmail.com>
2025-06-23 10:51:31 +0900

    ARCHIVE-71 feat: 사용자 관리 API 서비스 함수 추가

---
+++ src/services/api/endpoint.ts
@@ -0,0 +1,9 @@
+export const USER = {
+    LOGIN: 'v1/user/login',
+    LOGOUT: 'v1/user/logout',
+    CREATE_USER: 'v1/user',
+    GET_USER: (userId: number) => `v1/user/${userId}`,
+    UPDATE_USER: (userId: number) => `v1/user/${userId}`,
+    DELETE_USER: (userId: number) => `v1/user/${userId}`,
+    UPDATE_PASSWORD: (userId: number) => `v1/user/${userId}/password`,
+} as const;
