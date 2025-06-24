9f7f3835582d3e808dafb377c804765fe9681a1e
luna156 <63947262+luna156@users.noreply.github.com>
2025-06-24 20:38:42 +0900

    feat: shadcn card 추가

--- src/components/views/group/CreateGroupItem.vue
+++ src/components/views/group/CreateGroupItem.vue
@@ -1,16 +1,15 @@
 <template>
-    <Card
-      class="w-full max-w-xs h-32 flex items-center justify-center bg-muted cursor-pointer hover:shadow-lg transition"
-    >
-      <CardContent class="flex flex-col items-center justify-center text-muted-foreground">
-        <Plus class="w-8 h-8 mb-1" />
-        <span class="text-sm font-medium">그룹 생성</span>
-      </CardContent>
-    </Card>
-  </template>
-  
-  <script setup lang="ts">
-  import { Card, CardContent } from '@/components/ui/card';
-  import { Plus } from 'lucide-vue-next';
-  </script>
-  
+  <Card
+    class="bg-muted flex h-32 w-full max-w-xs cursor-pointer items-center justify-center transition hover:shadow-lg"
+  >
+    <CardContent class="text-muted-foreground flex flex-col items-center justify-center">
+      <Plus class="mb-1 h-8 w-8" />
+      <span class="text-sm font-medium">그룹 생성</span>
+    </CardContent>
+  </Card>
+</template>
+
+<script setup lang="ts">
+import { Card, CardContent } from '@/components/ui/card';
+import { Plus } from 'lucide-vue-next';
+</script>
