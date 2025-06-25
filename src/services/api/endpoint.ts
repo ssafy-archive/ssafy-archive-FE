a77dc1d562fd3f08f376c6fb91c1a0ab9f03e922
H4ngL <a01031367140@gmail.com>
2025-06-25 20:26:36 +0900

    ARCHIVE-71 refactor: api 엔드포인트에서 v1/ 제거

--- src/services/api/endpoint.ts
+++ src/services/api/endpoint.ts
@@ -1,9 +1,9 @@
 export const USER = {
-    LOGIN: 'v1/user/login',
-    LOGOUT: 'v1/user/logout',
-    CREATE_USER: 'v1/user',
-    GET_USER: (userId: number) => `v1/user/${userId}`,
-    UPDATE_USER: (userId: number) => `v1/user/${userId}`,
-    DELETE_USER: (userId: number) => `v1/user/${userId}`,
-    UPDATE_PASSWORD: (userId: number) => `v1/user/${userId}/password`,
+  LOGIN: 'user/login',
+  LOGOUT: 'user/logout',
+  CREATE_USER: 'user',
+  GET_USER: (userId: number) => `user/${userId}`,
+  UPDATE_USER: (userId: number) => `user/${userId}`,
+  DELETE_USER: (userId: number) => `user/${userId}`,
+  UPDATE_PASSWORD: (userId: number) => `user/${userId}/password`,
 } as const;
